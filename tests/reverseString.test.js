1293542f2767fe87811e03aa495dcb240e5abda4
Ikraam <IbnDaanis@Gmail.com>
2021-02-03 14:19:22 +0000

    Added more tests for reverseString

--- tests/reverseString.test.js
+++ tests/reverseString.test.js
@@ -1,5 +1,17 @@
 import reverseString from '../scripts/reverseString'
 
+test('The input should be a string.', () => {
+  expect(capitalize({ hi: [234] })).toBe('The input is not a string')
+})
+
+test('The input should not be empty', () => {
+  expect(capitalize('')).toBe('The input should not be empty')
+})
+
 test('Ikraam should be maarkI', () => {
   expect(reverseString('Ikraam')).toBe('maarkI')
+})
+
+test('Any character should work', () => {
+  expect(reverseString('0p;]')).toBe('];p0')
 })
