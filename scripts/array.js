b5351d394587e0383ed45a7b28b74d3d473d14c3
Ikraam <IbnDaanis@Gmail.com>
2021-02-03 13:26:41 +0000

    Added edge cases in array function

--- scripts/array.js
+++ scripts/array.js
@@ -1,13 +1,17 @@
 export default function array(arr) {
-  const sorted = arr.sort((a, b) => a - b)
-  const average = arr.reduce((acc, curr) => acc + curr, 0) / arr.length
-
-  return {
-    average,
-    min: sorted[0],
-    max: sorted[sorted.length - 1],
-    length: arr.length,
+  if (!Array.isArray(arr)) return 'Not an Array'
+  if (arr.every(item => item == +item)) {
+    const sorted = arr.sort((a, b) => a - b)
+    const average = arr.reduce((acc, curr) => acc + +curr, 0) / arr.length
+    return {
+      average,
+      min: sorted[0],
+      max: sorted[sorted.length - 1],
+      length: arr.length,
+    }
+  } else {
+    return 'Not numbers'
   }
 }
 
-console.log(array([1, 8, 3, 4, 2, 6]))
+console.log(array([1, '2', 3, 4, 2, 6]))
