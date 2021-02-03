05d2fa576977bb1275c4b738ab21e5b98260673e
Ikraam <IbnDaanis@Gmail.com>
2021-02-03 13:48:32 +0000

    Added tests for calculator

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,5 +1,33 @@
 import calculator from '../scripts/calculator'
 
-test('1 + 1 = 2', () => {
-  expect(calculator({ type: 'ADD', num1: 1, num2: 1 })).toBe(2)
+test('The type must be valid.', () => {
+  expect(calculator({ type: 'Hello', num1: 1, num2: 1 })).toBe(
+    "The type is not valid. Must be either 'ADD', 'SUBTRACT', 'MULTIPLY' or 'DIVIDE'."
+  )
+})
+
+test('Both input numbers must be numbers.', () => {
+  expect(calculator({ type: 'ADD', num1: 1, num2: ['1'] })).toBe(
+    'Either num1, num2, or both are not valid numbers.'
+  )
+})
+
+test('Test if string numbers work', () => {
+  expect(calculator({ type: 'ADD', num1: '1', num2: '1' })).toBe(2)
+})
+
+test('Testing Addition', () => {
+  expect(calculator({ type: 'ADD', num1: 2, num2: 2 })).toBe(4)
+})
+
+test('Testing Subtraction', () => {
+  expect(calculator({ type: 'SUBTRACT', num1: 20, num2: 1 })).toBe(19)
+})
+
+test('Testing Multiplication', () => {
+  expect(calculator({ type: 'MULTIPLY', num1: 10, num2: 2 })).toBe(20)
+})
+
+test('Testing Division', () => {
+  expect(calculator({ type: 'DIVIDE', num1: 10, num2: 2 })).toBe(5)
 })
